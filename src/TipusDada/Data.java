3107538d1cff6be83921b3388cb08b927910d159
ulkiobal <ulkiobal@users.noreply.github.com>
2021-01-19 16:25:11 +0100

    Clase data funcional

--- src/TipusDada/Data.java
+++ src/TipusDada/Data.java
@@ -1,4 +1,42 @@
 package TipusDada;
 
 public class Data {
+
+    private int dia;
+    private int mes;
+    private int any;
+
+    public Data(int dia, int mes, int any) {
+        this.dia = dia;
+        this.mes = mes;
+        this.any = any;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int any) {
+        this.any = any;
+    }
+
+    public String getDate() {
+        return dia + "/" + mes + "/" + any;
+    }
 }
