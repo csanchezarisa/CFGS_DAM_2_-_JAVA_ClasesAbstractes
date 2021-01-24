9ca88053830e97e4c5dd013c9e7a2e5411a0f3ec
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-01-24 17:59:59 +0100

    Pruebas pero sin funcionar bien jeje

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import TipusDada.Persona.Persona;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
+
+        Persona[] personas = GestionaLlistes.getPersonas();
 
     }
 }
